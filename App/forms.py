a4b0792aa51215a331ff4269814440a7d1367e1b
Udayanaik95 <naik.udaya25@gmail.com>
2023-04-25 14:21:22 +0530

    first commit

--- App/forms.py
+++ App/forms.py
@@ -1,13 +1,20 @@
 from django import forms
 
-def check_for_a(value):
-    if value[0].lower()=='a':
-        raise forms.ValidationError('Name Started With a')
+#def check_for_a(value):
+    #if value[0].lower()=='a':
+        #raise forms.ValidationError('Name Started With a')
     
+def check_for_a(value):
+    if value[0].lower()!='a':
+        raise forms.ValidationError('Name Started With a')
 
+#def check_for_len(value):
+    #if len(value)<6:
+        #raise forms.ValidationError('Length Is Less Than 6 Character')
+    
 def check_for_len(value):
-    if len(value)<6:
-        raise forms.ValidationError('Length Is Less Than 6 Character')
+    if len(value)>6:
+        raise forms.ValidationError('Length Is greater Than 6 Character')
 
 class StudentForm(forms.Form):
     #name=forms.CharField(max_length=100,validators=[check_for_a])
